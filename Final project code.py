5a32130c2e1e8d6fe44722e970f774081ba84c83
StudentJoel-web <55851271+StudentJoel-web@users.noreply.github.com>
2019-11-11 14:26:57 -0800

    Update Final project code.py

--- Final project code.py
+++ Final project code.py
@@ -1,6 +1,18 @@
-print("Welcome to the S quest")
+print("Joyous Holiday")
 input()
+#remember to recheck for my terriable spelling
 usrinput = input()
 print("please press enter")
 if usrinput == "":
-    print("")
+    print("Your in your bedroom you feel a sense of ease standing in it. You can see your project you working on and the video games you work on even more,Guess you can conisder yourself a hardworker. In front of you is the door, to your left your bed, right is a window.")
+if usrinput == "forward, front, walk forward, walk front, move forward, foward!":
+    print("You get out of your room and head downstairs, Your faimly are watching the tv or doing whatever. A nice warm feeling is in the air.")
+    #Cool now I just need to find out how to make this link to more text.
+if usrinput == "right, walk right, move right":
+    print("You look outide, its snowing with children playing in the snow. In the snow someone made a snow fort to protect themselfs from the snow airstike incoming. You quietly wish them luck.)
+   #Need to make it go back or change the direction, no idea how.
+if  usrinput == "left, move left, I don't feel like doing anything,":
+     print("You lay down in bed, the nerouvs unease of life creeps in your thoughts, you shoot them with your gun. Much better.You remember its the holidays however you might wander around make sure everyone enjoys themselfs.")
+ if usrinput == "behind,behind me, what about behind, you feel sercets crawlling on your back, backwards":
+     print("You look at the wall behind you, theres a framed award behind you. Award to dective S. For outstanding heroism and protecting the town.")
+           
